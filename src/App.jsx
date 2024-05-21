91a96df08d1d2d884524ac09dc79ea665354270c
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-05-21 22:49:35 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ function App() {
       <button onClick={()=>setColor("blue")}
        className ='flex items-center px-4 py-3 text-white bg-blue-500 hover:bg-blue-400 rounded m-10' style=  {{backgroundColor:"blue",color:text}}>blue</button>
 
-      <button onClick={ ()=>setColor("green") } className='flex items-center px-4 py-3 text-white bg-green-500 hover:bg-green-400 rounded m-10' style={{backgroundColor:"green",color:text}}>green</button>
+      <button onClick={ ()=>setColor("green") } className='flex items-center px-4 py-3
+       text-white bg-green-500 hover:bg-green-400 rounded m-10' style={{backgroundColor:"green",color:text}}>green</button>
 
        <button onClick={ ()=>setColor("yellow")} className='flex items-center px-4 py-3 text-white bg-yellow-500 hover:bg-yellow-400 rounded m-10 ' style={{backgroundColor:"yellow",color:text}}>yellow</button>
 
-      <button onClick={ ()=>setColor("red")} className='flex items-center px-4 py-3 text-white bg-red-500 hover:bg-red-400 rounded m-10' style={{backgroundColor:"red",color:text}}>red</button>
+      <button onClick={ ()=>setColor("red")} className='flex items-center px-4 py-3
+       text-white bg-red-500 hover:bg-red-400 rounded m-10' style={{backgroundColor:"red",color:text}}>red</button>
 
       </div>
     </>
